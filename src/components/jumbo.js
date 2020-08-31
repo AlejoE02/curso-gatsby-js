ebfa21cd13d8fcd9740a84e41b7b802503391b32
diego.reyes <diego.reyes@tars.dev>
2020-08-31 18:14:24 -0500

    Usando React para manejar la parte visual e interactiva de nuestra aplicación

---
+++ src/components/jumbo.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { StyledJumbo } from "../styles/components"
+export default function jumbo() {
+  return <StyledJumbo>
+    <div>
+      <h2> Consigue el mejor swag exclusivo y especial de Platzi!</h2>
+      <small>Piezas elegantes para los mejores estudiantes</small>
+    </div>
+  </StyledJumbo>
+}
