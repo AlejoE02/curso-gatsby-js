80ba9aef5d1130add1e76900288caa90b487d0ff
diego.reyes <diego.reyes@tars.dev>
2020-09-03 10:44:19 -0500

    Instalacion y configuracion de plugins

---
+++ src/utils/typography.js
@@ -0,0 +1,14 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: '1.16',
+  headerFontFamily:[
+    'Lato',
+    'Helvetica Neue',
+    'Arial'
+  ],
+  bodyFontFamily:['Open Sans', 'Roboto','Georgia']
+})
+
+export default typography
