1b121e967ea65d0d839a9168c69415ee359b1f08
diego.reyes <diego.reyes@tars.dev>
2020-09-04 11:44:08 -0500

    Plugins de Transformacion

--- src/components/jumbo.js
+++ src/components/jumbo.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { StyledJumbo } from "../styles/components"
-export default function Jumbo({description}) {
+import { Image } from "./"
+export default function Jumbo({ description }) {
   return (
     <StyledJumbo>
       <div>
         <h2> Consigue el mejor swag exclusivo y especial de Platzi!</h2>
         <small>{description}</small>
       </div>
+      <Image name="icon"/>
     </StyledJumbo>
   )
 }
